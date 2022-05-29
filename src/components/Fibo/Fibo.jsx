242969756c04b401a0394aea49fdb4f8aad7972b
Iván Moreno Mendoza <ivanmmendoza@hotmail.com>
2022-05-29 14:13:48 -0500

    Added Fibonacci Calculator

---
+++ src/components/Fibo/Fibo.jsx
@@ -0,0 +1,44 @@
+import React, {useState} from "react";
+
+const ItemCount = () => {
+    const [message, setMessage] = useState('');
+
+    const handleState = event => {
+        setMessage(event.target.value);
+    };
+
+    const calculateFibo = () => {
+        let fibonacci = [0, 1];
+
+        function getFibonacci(num) {
+            for (let i = 2; i < num; i++) {
+                fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+            }
+            return fibonacci;
+        }
+
+        // Push into the array
+        getFibonacci(message);
+
+        // Sum last two values of the array and return it
+        let newfibonacci = fibonacci.slice(-2), returnNumberFibonacci = newfibonacci[0] + newfibonacci[1];
+
+        getFibonacciNumber(returnNumberFibonacci);
+    }
+
+    
+    function getFibonacciNumber(data){
+        document.getElementsByClassName('fibo-container-result__results')[0].innerHTML = data;
+    }
+
+    return (
+        <div className="itemCounter">
+            <p className='fibo-container-title'>Calculate Fibonacci!</p>
+            <input name="message" value={message} onChange={handleState} className="fibo-container-input" type="number" placeholder='Add number...' ></input>
+            <button className="fibo-container-submit" onClick={calculateFibo}>Calculate</button>
+            <p className='fibo-container-result'>Fibonacci Result: <span className="fibo-container-result__results"></span></p>
+        </div>
+    );
+}
+
+export default ItemCount;
